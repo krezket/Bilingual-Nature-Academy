ae0ecbf6592a141f372efd90a1ff77c14cc5f65c
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-10-22 13:15:05 -0700

    added arrows and page indicators, added section 2

---
+++ scripts/slideshow.js
@@ -0,0 +1,10 @@
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
+
+leftArrow.addEventListener("click", () => {
+    console.log("left clicked")
+});
+
+rightArrow.addEventListener("click", () => {
+    console.log("right clicked")
+});
