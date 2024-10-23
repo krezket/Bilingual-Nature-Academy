de8947913501a3cb5669644c0eb89c734978dbb6
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-10-23 12:06:44 -0700

    added fonts, styled enroll button

---
+++ scripts/scroll.js
@@ -0,0 +1,14 @@
+let staticHeader = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    if (scrollPosition > 0) {
+        staticHeader.className = 'header-active';
+    }
+    else if (scrollPosition == 0) {
+        staticHeader.className = 'header';
+    }
+});
+
+
