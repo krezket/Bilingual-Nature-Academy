d60c40501c5f7efdebd69b5657d24e7e63e9463c
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-10-23 16:11:41 -0700

    saved

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,7 +3,7 @@ let staticHeader = document.querySelector('.header');
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     console.log(scrollPosition);
-    if (scrollPosition > 0) {
+    if (scrollPosition > 50) {
         staticHeader.className = 'header-active';
     }
     else if (scrollPosition == 0) {
