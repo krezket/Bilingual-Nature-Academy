86dbe7dfa543726b287e04f35ad836187343efeb
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-10-23 18:01:28 -0700

    added LLC

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,6 +3,7 @@ let staticHeader = document.querySelector('.header');
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     console.log(scrollPosition);
+
     if (scrollPosition > 50) {
         staticHeader.className = 'header-active';
     }
