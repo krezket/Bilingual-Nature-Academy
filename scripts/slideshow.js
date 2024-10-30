42a9a787f8d2eb83414b32d3d9a869b2eb5111a0
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-10-29 20:14:15 -0700

    built carousel

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,10 +1,44 @@
-const leftArrow = document.getElementById("left");
-const rightArrow = document.getElementById("right");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(index) {
+    let slides = document.querySelectorAll(".slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (index >= slides.length) slideIndex = 0;
+    if (index < 0) slideIndex = slides.length -1;
+    slides.forEach((slide) => (slide.style.display = "none"));
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+};
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlides(slideIndex);
+};
+
+// setInterval(() => {
+//     changeSlide(1);
+// }, 3000);
 
 leftArrow.addEventListener("click", () => {
+    changeSlide(-1);
     console.log("left clicked")
 });
 
 rightArrow.addEventListener("click", () => {
+    changeSlide(1);
     console.log("right clicked")
 });
